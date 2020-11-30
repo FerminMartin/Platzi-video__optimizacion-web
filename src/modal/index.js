b46a9f472949b131a26ca176f58e083088b7e42e
Fermin Martin <fermartorr01@gmail.com>
2020-12-01 00:52:36 +0100

    TAKING LISTENERS TO THE NEXT LEVEL
    -Set the listener with only one single event
    -Get the id of youtube Video creating a new property in dataset
    -In conclusion catch the id of any video with one single Evenlistener

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,7 +1,9 @@
 export const modalListener = event => {
   event.preventDefault()
-
   const img = event.target
   const link = img.parentElement
-  console.log(link.href)
+
+  if (link && link.classList.contains('js-video-link')) {
+    console.log(link.dataset.videoid)
+  }
 }
