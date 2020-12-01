a96e7f73d4f4c540b042ff36e36fdd089fb92112
Fermin Martin <fermartorr01@gmail.com>
2020-12-01 22:51:42 +0100

    LAZY LOADING
    -Analyze charge
    -Use import.then for webpack which divides the bundle
    -Rename bundles

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,11 +1,15 @@
-import { openModal } from './open-modal'
-
 export const modalListener = event => {
   event.preventDefault()
   const img = event.target
   const link = img.parentElement
+  const videoId = link.dataset.videoid
 
   if (link && link.classList.contains('js-video-link')) {
-    openModal(link.dataset.videoid)
+    //lazy loading
+    import(/*webpackChunkName: "modal"*/ './open-modal').then(
+      ({ openModal }) => {
+        openModal(videoId)
+      }
+    )
   }
 }
